6169b0bf5a2c795e1db6804415ddebc9b1c96591
admin <admin@DESKTOP-TD0JBJ1>
2022-09-24 03:14:51 +0300

    Добавьте файлы проекта.

---
+++ RegistrationWindow/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegistrationWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void signInButton(object sender, RoutedEventArgs e)
+        {
+            string Login,
+                Password;
+            Login = loginBox.Text;
+            Password = passwordBox.Text;
+            User user = new User();
+            bool result = user.VerificationUser(Login, Password);
+            if (result == true)
+            {
+                TimetableWindow Ttw = new TimetableWindow();
+                Ttw.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Повторите попытку.", "Ошибка авторизаци.", MessageBoxButton.OK , MessageBoxImage.Error);
+        }
+    }
+}
---
+++ RegistrationWindow/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationWindow
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static ObservableCollection<User> GetUserList()
+        {
+            var userList = new ObservableCollection<User>()
+            {
+                new User()
+                {
+                    Login = "p302",
+                    Password= "k80v"
+                }
+            };
+
+            return userList;
+        }
+
+        public bool VerificationUser( string Login, string Passvord)
+        {
+            foreach (var user in GetUserList())
+            {
+                if (user.Login == Login && (user.Password == Passvord))
+                    return true;
+                else return false;
+            }
+            return false;
+        }
+    }
+}
